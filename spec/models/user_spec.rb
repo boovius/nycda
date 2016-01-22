9ca3a3faa64d551e2dc6f0eb2c216218fd5cb0f9
Joshua Book <joshua.book@gmail.com>
2016-01-22 08:15:41 -0500

    add new context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,24 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'greet' do
 
-    it 'says hello u-s-e-r' do
-      #user = User.new
-      #name = 'zach'
-      #expect(user.greet name).to eq 'hello z-a-c-h'
-      expect(true).to eq true
+    context 'when user name is zach' do
+      it 'says hello u-s-e-r' do
+        #user = User.new
+        #name = 'zach'
+
+        #expect(user.greet name).to eq 'hello z-a-c-h'
+        expect(false).to eq true
+      end
     end
+
+    #context 'when user name is josh' do
+    #  it 'says hello u-s-e-r' do
+    #    user = User.new
+    #    name = 'josh'
+
+    #    expect(user.greet name).to eq 'hello j-o-s-h'
+    #    #expect(false).to eq true
+    #  end
+    #end
   end
 end
